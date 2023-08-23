310ce00eaf7c51dee9b7afb0b4953f8741b9756d
ProbablyStephan <104697876+ProbablyStephan@users.noreply.github.com>
2023-08-23 09:24:58 +0200

    Update list.py

--- list.py
+++ list.py
@@ -1,22 +1,15 @@
 import csv
 import os
 
+#two possibilities to access the mapping list of user to aircall ID.
+# 1. list as variable
+data = [["user1","aircall_ID1"],
+        ["user2","aircall_ID2"],
+        ["user3","aircall_ID3"]]
 
-data = [["thomas.krieninger","1055361"],
-        ["stephan.rott","1054671"],
-        ["igor.dymler","983875"],
-        ["anja.maerz","1055366"],
-        ["christian.herrmann","1055346"],
-        ["driton.kola","1055360"],
-        ["ferdinand.herrmann","1055349"],
-        ["horst.sueffert","1055313"],
-        ["julia.schwaiger","1055277"],
-        ["katrin.loeffler","1055277"],
-        ["marina.weweck","1055331"],
-        ["markus.croseck","1055334"],
-        ["nadege.viaud","1055322"],
-        ["benjamin.sueffert","1055315"]]
-
+# 2. open a .csv on a networkdrive. (possible problems when using over VPN)
+with open(r"Full Qualified Name of the filelocation", newline="") as csvfile:
+    data = list(csv.reader(csvfile,delimiter=","))
 
 def getID(name):
     for x in data:

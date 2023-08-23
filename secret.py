599b8e155ade16a403275eeec5786b0cf234adf6
ProbablyStephan <104697876+ProbablyStephan@users.noreply.github.com>
2023-08-23 10:10:49 +0200

    Update secret.py

--- secret.py
+++ secret.py
@@ -1,1 +1,9 @@
+#to hide the API Token its saved in a user enviromental variable (rolled out to machines via GPO)
+#
 
+
+import os
+
+def getHeaderLocal():
+    tmp = "Basic " + os.environ.get("NAME OF VARIABLE")
+    return {"Authorization": tmp, "Content-Type": "application/json"}

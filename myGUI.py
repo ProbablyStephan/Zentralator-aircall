9fe2f09bae754651a6678eaac4c6af9dbb127d1e
ProbablyStephan <104697876+ProbablyStephan@users.noreply.github.com>
2023-08-24 14:59:11 +0200

    Update myGUI.py

--- myGUI.py
+++ myGUI.py
@@ -1,7 +1,10 @@
 import wx
-import os
 import apiCalls
+import dataset
 from datetime import datetime
+
+helptxt = f'Funktionen: \n Überprüfen - Ermittelt ob sich der Erkannte Benutzer in der Zentrale befindet \n Einchecken - Der erkannte Benuzer tritt dem Team Zentrale bei \n Auschecken - Der erkannte Benutzer verlässt die Zentrale unter der Bedingung das noch genug Mitarbeiter in der Zentrale sind. \n Mitgleider - Listet die akutellen Mitglieder in der Zentrale auf'
+
 
 class MainPanel(wx.Panel):
     def __init__(self, parent):
@@ -13,23 +16,25 @@ class MainPanel(wx.Panel):
 
         #Currently Detected User
         currentDesc = wx.StaticText(self,label = "Erkannter Benutzer")
-        self.currentUser = wx.TextCtrl(self, size = (320,22), style = wx.TE_READONLY)
+        self.recognizedUser = wx.TextCtrl(self, size = (400,22), style = wx.TE_READONLY)
 
         #Eventlogger
-        self.logger = wx.TextCtrl(self,size = (320,300), style = wx.TE_MULTILINE | wx.TE_READONLY)
+        self.logger = wx.TextCtrl(self,size = (400,300), style = wx.TE_MULTILINE | wx.TE_READONLY)
 
         #Buttoms
-        ButtomCheck = wx.Button(self, label = "Überprüfen")
-        ButtomIn = wx.Button(self, label = "Einchecken")
-        ButtomOut = wx.Button(self, label =  "Auschecken")
+        self.ButtomCheck = wx.Button(self, label = "Überprüfen")
+        self.ButtomIn = wx.Button(self, label = "Einchecken")
+        self.ButtomOut = wx.Button(self, label =  "Auschecken")
+        self.ButtomMember = wx.Button(self, label = "Mitglieder")
 
-        self.Bind(wx.EVT_BUTTON, self.OnCheck, ButtomCheck)
-        self.Bind(wx.EVT_BUTTON, self.OnIn, ButtomIn)
-        self.Bind(wx.EVT_BUTTON, self.OnOut, ButtomOut)
+        self.Bind(wx.EVT_BUTTON, self.OnCheck, self.ButtomCheck)
+        self.Bind(wx.EVT_BUTTON, self.OnIn, self.ButtomIn)
+        self.Bind(wx.EVT_BUTTON, self.OnOut, self.ButtomOut)
+        self.Bind(wx.EVT_BUTTON, self.OnMember, self.ButtomMember)
 
         #Grid Layout
-        buttomSizer.AddMany([(ButtomCheck),(ButtomIn),(ButtomOut)])
-        flexSizer.AddMany([(currentDesc),(self.currentUser),(buttomSizer),(self.logger)])
+        buttomSizer.AddMany([(self.ButtomCheck),(self.ButtomIn),(self.ButtomOut),(self.ButtomMember)])
+        flexSizer.AddMany([(currentDesc),(self.recognizedUser),(buttomSizer),(self.logger)])
         flexSizer.AddGrowableCol(1,1)
         flexSizer.AddGrowableRow(2,1)
         mainSizer.Add(flexSizer, proportion = 2, flag = wx.ALL | wx.EXPAND, border = 15)
@@ -38,25 +43,43 @@ class MainPanel(wx.Panel):
 
     #Funtctions
     def OnCheck(self, e):
-        self.logger.AppendText(datetime.now().strftime("%H:%M:%S") +" - "+ apiCalls.teamCheck()+"\n")
+        self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - {str(apiCalls.teamCheck(apiCalls.currentUser))}\n')
         return
     
     def OnIn(self, e):
-        self.logger.AppendText(datetime.now().strftime("%H:%M:%S") +" - "+ apiCalls.teamPost()+"\n")
+        self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - {str(apiCalls.addToTeam(apiCalls.currentUser))}\n')
         return
     
     def OnOut(self, e):
-        self.logger.AppendText(datetime.now().strftime("%H:%M:%S") +" - "+ apiCalls.guardedDelete()+"\n")
+        self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - {str(apiCalls.complexeDelete(apiCalls.currentUser))}\n')
         return
     
+    def OnMember(self,e):
+        data = apiCalls.listMembers()
+        if isinstance(data, list):
+            self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - Die {len(data)} Mitglieder sind: \n')
+            for x in data:
+                self.logger.AppendText(f'{x} \n')
+        else:
+            self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - {data}')
+
+
     def Init(self):
-        self.currentUser.AppendText(os.environ.get("Username"))
+        self.recognizedUser.AppendText(f'{apiCalls.currentUser} - {dataset.getDep(apiCalls.currentUser)}')
+        if dataset.getDep(apiCalls.currentUser) == "unknown user":
+            self.ButtomCheck.Disable()
+            self.ButtomIn.Disable()
+            self.ButtomOut.Disable()
+            self.ButtomMember.Disable()
+            self.logger.AppendText(f'{datetime.now().strftime("%H:%M:%S")} - {apiCalls.currentUser} hat keine Berechtigungen das Programm zu verwenden.')
+
+            
 
             
 
 class MainWindow(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, parent = None, title = "Zentrale - Rein & Raus", size=(500,500))
+        wx.Frame.__init__(self, parent = None, title = "Zentrale - Rein & Raus", size=(560,500))
 
         self.panel = MainPanel(self)
         
@@ -82,14 +105,15 @@ class MainWindow(wx.Frame):
         self.Close(True) #close the Frame
 
     def OnAbout(self,e):
-        dlg = wx.MessageDialog(self, "IT","Informationen über dieses Programm")
+        dlg = wx.MessageDialog(self, f'geschrieben für B.E.S.T. Fluidsysteme GmbH München \n von Stephan Rott \n Version: v.4   -   24.08.2023' ,"Informationen über dieses Programm")
         dlg.ShowModal()
         dlg.Destroy()
     
     def OnHelp(self,e):
-        dlg = wx.MessageDialog(self, "Bei Problemen an die IT wenden","Hilfe")
+        dlg = wx.MessageDialog(self, helptxt,"Hilfe")
         dlg.ShowModal()
         dlg.Destroy()
 
     def InitMain(self):
+        apiCalls.currentUser = apiCalls.initUser()
         MainPanel.Init(self.panel)
